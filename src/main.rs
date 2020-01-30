c257e0a67dc20ac547c7027bb7c171d16d4a3e11
LittleSoap <32540199+LittleSoap@users.noreply.github.com>
2020-01-30 20:48:35 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+use clap::{App, Arg};
+use structopt::StructOpt;
+use std::process::Command;
+use std::string::String;
+
+#[derive(StructOpt)]
+struct Cli {
+    command1: String,
+    command2: String
 }
+
+fn main() { 
+
+    let args = Cli::from_args();
+
+    //Get command lines
+    
+    let command1 = &args.command1;
+    let command2 = &args.command2;
+
+    //First command 
+    let ouptut1 = Command::new(command1)
+                    .output()
+                    .expect("First command failed");
+
+    let outputString = String::from_utf8_lossy(&ouptut1.stdout);
+    let mut split = outputString.split(" ");
+
+    //Second command
+    Command::new(command2)
+        .args(split)
+        .spawn()
+        .expect("Second command failed");
+}
+
